e70b3e72f55f6207c88cc4a40cf6e2b31e105fef
owuordove@gmail.com <owuordove@gmail.com>
2022-05-28 11:28:04 +0300

    edit

--- Diamond.py
+++ Diamond.py
@@ -1,11 +1,11 @@
-rows = 5
+rows = 10
 k = 2 * rows - 2
 for i in range(0, rows):
     for j in range(0, k):
         print(end=" ")
     k = k - 1
     for j in range(0, i + 1):
-        print("* ", end="")
+        print("+ ", end="")
     print("")
     
 k = rows - 2
