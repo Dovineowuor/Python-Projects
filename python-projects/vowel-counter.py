6d2eb17e9b4e4cd1c8e905becd5bf896de5a461c
owuordove@gmail.com <owuordove@gmail.com>
2022-05-30 14:13:35 +0300

    vowel counter

--- python-projects/vowel-counter.py
+++ python-projects/vowel-counter.py
@@ -1,11 +1,14 @@
-prophecy = "And there shall in that time be rumours of things going astray, and there will be a great confusion as to where things really are, and nobody will really know where lieth those little things with the sort of raffia work base, that has an attachment…at this time, a friend shall lose his friends’s hammer and the young shall not know where lieth the things possessed by their fathers that their fathers put there only just the night before around eight o’clock…"
+from fileinput import filename
+
+
+prophecy = input(filename())
 
 vowel_count = 0
 
 # TODO: set the value of vowel_count to be the number of vowels in prophecy
 
 
-# This is my vowel counting code:
+# :
 
 vowel_count += prophecy.count('a')
 vowel_count += prophecy.count('A')
